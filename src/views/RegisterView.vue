67e2cab160a28c2b9a68c57c9e73b8132f3b093d
骆俊杰 <1705129148@qq.com>
2025-12-07 01:18:03 +0800

    fix（修正role值）

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -11,11 +11,11 @@ const props = { value: 'id', label: 'role', disabled: 'unable' }
 
 const options = [
   {
-    id: 0,
+    id: 1,
     role: '用户',
   },
   {
-    id: 1,
+    id: 2,
     role: '管理员',
   },
 ]
